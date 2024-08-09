5f0e8714f448113d28a57982c0b820d5712c13a7
Abishek4625 <abhishektenzz7@gmail.com>
2024-08-09 16:31:42 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
